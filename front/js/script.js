5d6c62b3e1d404c435c78c43ed3b3817138db175
Totorusse <victordruhot@gmail.com>
2021-12-02 13:42:37 +0100

    fin JS index

--- front/js/script.js
+++ front/js/script.js
@@ -8,32 +8,33 @@ fetch("http://localhost:3000/api/products")
     console.log(value);
     for (let i of value) {
       console.log(i);
-       document.getElementById("items").innerHTML +=
-         "<a href=" +
-         "./product.html?id=" + i.id +
-         ">" +
-         "<article>" +
-         "<img src=" +
-         i.imageUrl +
-         " alt=" +
-         "Lorem ipsum dolor sit amet," + i.name +
-         ">" +
-         "<h3 class=" +
-         "productName" +
-         ">" +
-         i.name +
-         "</h3>" +
-         "<p class=" +
-         "productDescription" +
-         ">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>" +
-         "</article>" +
-         "</a>";
+      document.getElementById("items").innerHTML +=
+        "<a href=" +
+        "./product.html?id=" +
+        i._id +
+        ">" +
+        "<article>" +
+        "<img src=" +
+        i.imageUrl +
+        " alt=" + '"' +
+        i.altTxt + '"' +
+        ">" +
+        "<h3 class=" +
+        "productName" +
+        ">" +
+        i.name +
+        "</h3>" +
+        "<p class=" +
+        "productDescription" +
+        ">" +
+        i.description +
+        "</p>" +
+        "</article>" +
+        "</a>";
     }
   })
   .catch(function (err) {
     // Une erreur est survenue
   });
 
-
-  
 
