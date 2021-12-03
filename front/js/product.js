b02ae0e3e86533526da3ab15e15a131c9369d907
Totorusse <victordruhot@gmail.com>
2021-12-03 17:59:08 +0100

    Ajout JS page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,7 @@
+let str = document.URL;
+let url = new URL(str);
+let LienId = url.searchParams.get("id");
+
 fetch("http://localhost:3000/api/products")
   .then(function (res) {
     if (res.ok) {
@@ -6,10 +10,18 @@ fetch("http://localhost:3000/api/products")
   })
   .then(function (value) {
     console.log(value);
-  
     for (let i of value) {
-      console.log(i);
-      document.getElementsByClassName("item__img")[0].innerHTML += i; 
+      //console.log(i);
+      if (i._id == LienId) {
+        document.getElementsByClassName("item__img")[0].innerHTML +=
+          "<img src=" + i.imageUrl + " alt=" + '"' + i.altTxt + '"' + ">";
+        document.getElementById("title").innerHTML = i.name;
+        document.getElementById("price").innerHTML = i.price;
+        document.getElementById("description").innerHTML = i.description;
+        for (let j of i.colors) {
+          document.getElementById("colors").innerHTML += "<option value=" + j + ">" + j + "</option>";
+        }
+      }
     }
   })
   .catch(function (err) {
